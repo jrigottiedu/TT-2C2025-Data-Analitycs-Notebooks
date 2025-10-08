638275d62a91c72f36141f5c7787abdaf7e111b2
alex-degarate <alex-degarate@users.noreply.github.com>
2025-10-08 18:52:54 -0300

    Delete practica directory

--- practica/ventas/test.py
+++
@@ -1,9 +0,0 @@
-
-# LEER LIBREOFFICE CALC.ODS 
-import pandas as pd
-
-# ESPECIFICAR ODF
-print("\nLEER LIBREOFFICE DE CALC 'ventas.ods' -  engine='odf' \n")
-df3 = pd.read_excel('ventas.ods', engine='odf')
-
-print( df3.head())
